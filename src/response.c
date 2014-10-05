0b49c15b3d84cf5798914cb8d5175114086c7f5a
Nicolas Pierre-Loti-Viaud <nicoplv.auto@gmail.com>
2014-10-05 05:40:03 +0200

    Add Profil Photo

--- src/response.c
+++ src/response.c
@@ -8,11 +8,14 @@ static Window *window_response;
 static GFont lato_font;
 
 static TextLayer *window_response_text_name_layer;
+static BitmapLayer *window_response_profil_layer;
 
 static ActionBarLayer *window_response_action_bar_layer;
 
 static GBitmap *image_add_contact_bitmap;
 static GBitmap *image_addno_contact_bitmap;
+
+static GBitmap *image_profile_bitmap;
 
 void add_contact_valid(const char *const name){
 	vibes_short_pulse();
@@ -51,13 +54,28 @@ static void window_response_load(Window *window) {
 	// Create Bitmap
 	image_add_contact_bitmap = gbitmap_create_with_resource(RESOURCE_ID_IMAGE_ADD_CONTACT_ACTION_BAR);
 	image_addno_contact_bitmap = gbitmap_create_with_resource(RESOURCE_ID_IMAGE_ADDNO_CONTACT_ACTION_BAR);
+	switch(time_ms(NULL, NULL)%3){
+		case 0:
+			image_profile_bitmap = gbitmap_create_with_resource(RESOURCE_ID_IMAGE_PROFIL_BATMAN);
+		break;
+		case 1:
+			image_profile_bitmap = gbitmap_create_with_resource(RESOURCE_ID_IMAGE_PROFIL_IRONMAN);
+		break;
+		default:
+			image_profile_bitmap = gbitmap_create_with_resource(RESOURCE_ID_IMAGE_PROFIL_CAPTAINAMERICA);
+		break;
+	}
 	// Create Name Text Layer
-	window_response_text_name_layer = text_layer_create(GRect(0, 120, 144, 25));
+	window_response_text_name_layer = text_layer_create(GRect(0, 120, 124, 25));
 	text_layer_set_font(window_response_text_name_layer, lato_font);
 	text_layer_set_text_color(window_response_text_name_layer, GColorBlack);
 	text_layer_set_text_alignment(window_response_text_name_layer, GTextAlignmentCenter);
 	text_layer_set_text(window_response_text_name_layer, "Name");
 	layer_add_child(window_get_root_layer(window_response), text_layer_get_layer(window_response_text_name_layer));
+	// Create Profile Image Layer
+	window_response_profil_layer = bitmap_layer_create(GRect(10, 10, 100, 100));
+	bitmap_layer_set_bitmap(window_response_profil_layer, image_profile_bitmap);
+	layer_add_child(window_get_root_layer(window_response), bitmap_layer_get_layer(window_response_profil_layer));
 	// Create Action Bar Layer
 	window_response_action_bar_layer = action_bar_layer_create();
 	action_bar_layer_add_to_window(window_response_action_bar_layer, window);
@@ -69,7 +87,11 @@ static void window_response_load(Window *window) {
 static void window_response_unload(Window *window) {
 	fonts_unload_custom_font(lato_font);
 	text_layer_destroy(window_response_text_name_layer);
+	bitmap_layer_destroy(window_response_profil_layer);
 	action_bar_layer_destroy(window_response_action_bar_layer);
+	gbitmap_destroy(image_add_contact_bitmap);
+	gbitmap_destroy(image_addno_contact_bitmap);
+	gbitmap_destroy(image_profile_bitmap);
 }
 
 void init_response() {
